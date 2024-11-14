0c4764a257173da0f0242471f84a93cac2edd6af
Nathan Solomon Zegeye <solomonnathan065@gmail.com>
2024-11-14 18:03:26 +0300

    Day 4

---
+++ AdvancedReact.js
@@ -0,0 +1,31 @@
+// Keys 
+// - Uniques identifiers used in lists 
+// - Used to identify which items have been changed, added or removed 
+// - Use stable identifiers for lists
+
+// Uncontrolled components
+// - DOM handles form data 
+// Controlled components
+// - Components state handles form data
+// - Has value attribute attached to a state 
+
+// Props and state
+// - Both are plain javascript objects react uses to store data
+// - Both trigger render updates
+// Stateless component only have props 
+// Stateful components have both props and state 
+// - Props are immutable data sent from parent to child (immutable for the child),
+// they are a components configuration
+// - Props are values passed to component 
+// - So state is a serialize double representation of one point in time 
+// or in other words a snapshot A component manages its own state internally
+// - States are managed within the component
+
+// Context API solution for props drilling 
+
+
+// Pure and Impure function 
+// Functions that have side effects are impure
+// A pure function should receive specific input. 
+// That is a specific parameter will always return the exact same output. 
+// No matter how many times it gets invoked.
